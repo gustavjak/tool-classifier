5a6aeeba254ce04779e097fe75d8c32fba597574
Gustav Jakobsson <gustav.jakobsson@ai.se>
2023-09-27 11:14:39 +0200

    Initial commit

---
+++ tool-classifier/tool_classifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  tool_classifierApp.swift
+//  tool-classifier
+//
+//  Created by MITC on 2023-09-27.
+//
+
+import SwiftUI
+
+@main
+struct tool_classifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
