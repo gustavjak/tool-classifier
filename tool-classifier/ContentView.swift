44ece8e14aca3d054c0287d46ef158b292771ef3
AugustWanger <j.augustwanger@gmail.com>
2023-11-09 10:34:54 +0100

    Pushing code

--- tool-classifier/ContentView.swift
+++ tool-classifier/ContentView.swift
@@ -8,37 +8,4 @@
 import SwiftUI
 import AVFoundation
 
-class ViewController: UIViewController {
-    @IBOutlet var imageView: UIImageView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    @IBAction func didTapButton() {
-        let vc = UIImagePickerController()
-        vc.sourceType = .photoLibrary
-        vc.delegate = self
-        vc.allowsEditing = true
-        present(vc, animated: true)
-    }
-}
 
-extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        
-        let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
-            imageView.image = image
-        }
-        
-        picker.dismiss(animated: true, completion: nil)
-
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
-    }
-    
-    
-}
--- tool-classifier/tool_classifierApp.swift
+++ tool-classifier/tool_classifierApp.swift
@@ -6,12 +6,54 @@
 //
 
 import SwiftUI
+import CoreML
+import Vision
 
-@main
-struct tool_classifierApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ViewController()
+struct ContentView: View {
+    @State private var image: Image?
+    @State private var classificationResult: String = "Select an image"
+    private let model = ToolClassifier_1(model: <#T##MLModel#>);
+    
+    var body: some View {
+        VStack {
+            Text(classificationResult)
+            
+            image?
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+            
+            Button("Select Image") {
+                self.classificationResult = "Classifying..."
+                self.image = nil
+                // Present an image picker or load an image from your data source
+            }
         }
+    }
+    
+    func classifyImage(uiImage: Image) -> VNCoreMLModel {
+        let defaultConfig = MLModelConfiguration()
+
+
+        // Create an instance of the image classifier's wrapper class.
+        let imageClassifierWrapper = try? ToolClassifier_1(configuration: defaultConfig)
+
+
+        guard let imageClassifier = imageClassifierWrapper else {
+            fatalError("App failed to create an image classifier model instance.")
+        }
+
+
+        // Get the underlying model instance.
+        let imageClassifierModel = imageClassifier.model
+
+
+        // Create a Vision instance using the image classifier's model instance.
+        guard let imageClassifierVisionModel = try? VNCoreMLModel(for: imageClassifierModel) else {
+            fatalError("App failed to create a `VNCoreMLModel` instance.")
+        }
+
+
+        return imageClassifierVisionModel;
     }
 }
